1c0d21f34f63032958ce978aba8755aa7950018f
User_Pc\User01 <supinya.dawd@bumail.net>
2017-03-24 14:58:04 +0700

    14. Counting points and displaying the score

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,14 +6,45 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
-	void start () {
-		SpawnWaver ();
+	public GUIText scorerText;
+	private int score;
+
+	void Start () {
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaver ());
 	}
 
-	void SpawnWaver (){
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+	IEnumerator SpawnWaver ()
+	{
+
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+
+	
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scorerText.text = "Score: " + score;
+	}
+
 }
